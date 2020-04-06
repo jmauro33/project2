064ce97f1e45f75d51bc59c6a42f153b69c82922
Sheri Rhoades <sheri_rhoades@yahoo.com>
2020-04-06 13:37:24 -0500

    kept getting error .get not a function, added jquery script src on login.html again, seems to be working now

--- public/js/login.js
+++ public/js/login.js
@@ -1,16 +1,19 @@
-$(document).ready(function() {
+$(document).ready(function () {
   // Getting references to our form and inputs
-  var loginForm = $("form.login");
+  var loginButton = $("#loginButton");
   var emailInput = $("input#email-input");
   var passwordInput = $("input#password-input");
 
   // When the form is submitted, we validate there's an email and password entered
-  loginForm.on("submit", function(event) {
+  loginButton.on("click", function (event) {
+    console.log("loginbtn clicked");
     event.preventDefault();
     var userData = {
       email: emailInput.val().trim(),
-      password: passwordInput.val().trim()
+      password: passwordInput.val().trim(),
     };
+
+    console.log("userdata: " + userData);
 
     if (!userData.email || !userData.password) {
       return;
@@ -24,15 +27,17 @@ $(document).ready(function() {
 
   // loginUser does a post to our "api/login" route and if successful, redirects us the the lost pet profile page
   function loginUser(email, password) {
-    $.post("/api/login", {
+    console.log(email, password, "userlogin");
+    $.get("/api/login", {
       email: email,
-      password: password
+      password: password,
     })
-      .then(function() {
-        window.location.replace("/lostpet");
+      .then(function (response) {
+        console.log(response, "hello");
+        window.location.replace("/addlostpet.html");
         // If there's an error, log the error
       })
-      .catch(function(err) {
+      .catch(function (err) {
         console.log(err);
       });
   }
